7d5e9417562771dff2011546bccccc301842c5b9
Stanislav <ictstas@gmail.com>
2022-02-27 16:27:41 -0800

    Returning to project after busy week

---
+++ 2_UART/2_UART_TX/Debug/Src/main.d
@@ -0,0 +1,4 @@
+Src/main.o: ../Src/main.c \
+ F:/engineering/codespace/bare_metal/Bare_Metal_L476/1_GPIO/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h
+
+F:/engineering/codespace/bare_metal/Bare_Metal_L476/1_GPIO/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
