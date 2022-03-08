8ba2eab030378a813ec914c13825ea8c6aca388c
StanislavEng <ictstas@gmail.com>
2022-03-07 23:08:24 -0800

    Finished UART intro

---
+++ 2_UART/2_UART_RX/Debug/Src/main.d
@@ -0,0 +1,27 @@
+Src/main.o: ../Src/main.c ../Inc/uart.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+
+../Inc/uart.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/core_cm4.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/TESTING_CL/Documents/Engineering/stm32/bare\ metal/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
